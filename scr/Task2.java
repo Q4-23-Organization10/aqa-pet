31b12d1f13b8b6108d6759366c358cc81935e535
YanaBabenko <babenkoyana159@gmail.com>
2023-12-20 21:33:36 +0200

    add task1,task2,task3

---
+++ scr/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        /*Формули:
+        Celsius = 5/9 (F - 32)
+        Kelvin = C + 273.16*/
+
+        double X = 25;
+        double Y = X * (9.0/5.0) + 32;
+        double Z = X + 273.16;
+
+        System.out.println(X + " Celsius; " + Y + " Fahrenheit; " + Z + " Kelvin;");
+    }
+}
---
+++ scr/Task3a.java
@@ -0,0 +1,12 @@
+public class Task3a {
+    public static void main(String[] args) {
+
+        // Перша сторона а
+        double a = 12.25;
+        // Друга сторона b
+        double b = 8;
+        double S = a * b / 2;
+
+        System.out.printf( "Площа трикутника = " + S);
+    }
+}
