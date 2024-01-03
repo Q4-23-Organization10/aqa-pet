141b8a62302eeeff28cee39eca0f6c2668dd3200
YanaBabenko <babenkoyana159@gmail.com>
2024-01-03 11:05:28 +0200

    add hw7 task 3-5

--- hw7/HW7task3.java
+++ hw7/HW7task3.java
@@ -1,8 +1,19 @@
+import java.util.Random;
+
 public class HW7task3 {
     public static void main(String[] args) {
+        Random random = new Random();
 
-        int [] array = {63, -82, 11, -2, 99, 8, 155, 579, 76};
+        int [] array = new int[8];
 
+        System.out.println("Початковий масив:");
+
+        for (int i = 0; i < array.length; i++) {
+            array [i]= random.nextInt(100);
+            System.out.println(array [i]);
+        }
+
+        System.out.println("Змінений масив:");
 
         for (int i = 0; i < array.length; i++) {
             if ( array [i] % 2 == 0)
