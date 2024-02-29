b8560c77801e6ca3db906b5210fac2c6eaa86b0a
YanaBabenko <babenkoyana159@gmail.com>
2024-02-29 13:53:29 -0500

    Edit hw 14

---
+++ hw14/src/main/java/PlayerHuman.java
@@ -0,0 +1,39 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+
+public class PlayerHuman extends Player {
+
+    public static final Logger LOGGER = LogManager.getLogger(PlayerHuman.class);
+    public Scanner scanner;
+
+    public PlayerHuman(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+
+
+    @Override
+    public Move getMove() {
+        System.out.print("Ваш вибір (1. камінь, 2. ножиці, 3. папір): ");
+        System.out.print("Введіть одну цифру: ");
+        int userint = scanner.nextInt();
+            switch (userint) {
+                case 1:
+                    LOGGER.info("LOG:Гравець вибрав камінь");
+                    return Move.ROCK;
+                case 2:
+                    LOGGER.info("LOG:Гравець вибрав ножиці");
+                    return Move.SCISSORS;
+                case 3:
+                    LOGGER.info("LOG:Гравець вибрав папір");
+                    return Move.PAPER;
+                default:
+                    LOGGER.warn("LOG:Неправильний вибір");
+                    throw new IllegalArgumentException("Неправильний вибір");
+            }
+    }
+}
