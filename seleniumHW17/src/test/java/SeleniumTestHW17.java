b4f1a06025d907c0ac235bc01dac37801f4bd92b
YanaBabenko <babenkoyana159@gmail.com>
2024-03-29 07:23:52 -0400

    Edit hw 14

---
+++ seleniumHW17/src/test/java/SeleniumTestHW17.java
@@ -0,0 +1,41 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SeleniumTestHW17 {
+
+    private WebDriver driver;
+    private final String URL = "https://the-internet.herokuapp.com/login";
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+
+
+    @AfterEach
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+
+    @AfterEach
+    public void setDown() {
+
+        driver.quit();
+    }
+
+}
